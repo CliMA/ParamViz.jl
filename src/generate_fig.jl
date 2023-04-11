2dd85d3f71fe862a4e4e920e9c600525dfdb7258
AlexisRenchon <a.renchon@gmail.com>
2023-04-10 17:33:04 -0700

    WIP

--- src/generate_fig.jl
+++ src/generate_fig.jl
@@ -94,14 +94,22 @@ function parameterisation(inputs::Inputs)
     return p1*sin(x) + p2*sin(y) + c1 + c2
 end
 
+function parameterisation(x, y, p1, p2, c1, c2) # most CliMA function are defined like that...
+  return p1*sin(x) + p2*sin(y) + c1 + c2
+end
+
+function parameterisation(x, y, p) # easy to rewrite like this to call mat function
+  c1, c2 = inputs.constants.values[1], inputs.constants.values[2]
+  return p[1]*sin(x) + p[2]*sin(y) + c1 + c2
+end
+
 drivers = Drivers(("x", "y"), (1, 1), ([-5, 5], [-5, 5]))
 parameters = Parameters(("p1", "p2"), (1.0, 1.0), ([-5, 5], [-5, 5]))
 constants = Constants(("c1", "c2"), (1.0, 1.0))
 inputs = Inputs(drivers, parameters, constants)
 parameterisation(inputs)
-
-
-
+parameterisation(inputs.drivers.values[1], inputs.drivers.values[2], inputs.parameters.values[1], inputs.parameters.values[1], inputs.constants.values[1], inputs.constants.values[1])
+parameterisation(inputs.drivers.values[1], inputs.drivers.values[2], [inputs.parameters.values[1], inputs.parameters.values[2]])
 
 # Then call param_dashboard(parameterisation::Function, inputs::Inputs)
 
