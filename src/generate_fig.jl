7bd70312f988345151e458d2ab0f33ff787af227
AlexisRenchon <a.renchon@gmail.com>
2023-05-17 14:04:08 -0700

    many viz tweaks

--- src/generate_fig.jl
+++ src/generate_fig.jl
@@ -8,7 +8,7 @@ function param_dashboard(parameterisation::Function, inputs::Inputs, drivers_sli
   fig = Figure(resolution = (1200, 1200))
 
   # JSServe layout
-  ax3D = Axis3(fig[1,1], xlabel = inputs.drivers.names[1], ylabel = inputs.drivers.names[2], zlabel = output.name); zlims!(ax3D, output.range)
+  ax3D = Axis3(fig[1,1:2][1,1], xlabel = inputs.drivers.names[1], ylabel = inputs.drivers.names[2], zlabel = output.name); zlims!(ax3D, output.range)
   ax_d1 = Axis(fig[2,1], xlabel = inputs.drivers.names[1], ylabel = output.name); ylims!(ax_d1, output.range)
   ax_d2 = Axis(fig[2,2], xlabel = inputs.drivers.names[2], ylabel = output.name); ylims!(ax_d2, output.range)
 
@@ -18,7 +18,9 @@ function param_dashboard(parameterisation::Function, inputs::Inputs, drivers_sli
   parameters_vals = [parameters_sliders[i].value for i in 1:n_parameters] |> Tuple 
 
   steps = 30
+  drivers = lift((args...,) -> args, drivers_vals...)
   parameters = lift((args...,) -> args, parameters_vals...)
+  constants = inputs.constants.values
 
   x_d1 = collect(range(inputs.drivers.ranges[1][1], inputs.drivers.ranges[1][2], steps)) # min d1 to max d1, n steps
   x_d2 = collect(range(inputs.drivers.ranges[2][1], inputs.drivers.ranges[2][2], steps)) # min d2 to max d2, n steps
@@ -31,7 +33,8 @@ function param_dashboard(parameterisation::Function, inputs::Inputs, drivers_sli
   x = @lift(mat(parameterisation, inputs, $parameters, steps)[1]) 
   y = @lift(mat(parameterisation, inputs, $parameters, steps)[2])
   z = @lift(mat(parameterisation, inputs, $parameters, steps)[3])
-  surface!(ax3D, x, y, z, colormap = Reverse((:Spectral, 0.6),), transparency = true, alpha = 0.2, shading = false)
+  surface!(ax3D, x, y, z, colormap = Reverse((:Spectral, 0.8),), transparency = true, alpha = 0.2, shading = false, colorrange = output.range) 
+  Colorbar(fig[1, 1:2][1, 2], colormap = Reverse(:Spectral), limits = output.range, label = output.name)
 
   # Plot 2D lines of model(drivers, params)
   lines!(ax_d1, x_d1, y_d1, color = :red, linewidth = 4)
@@ -41,9 +44,14 @@ function param_dashboard(parameterisation::Function, inputs::Inputs, drivers_sli
   lines!(ax3D, x_d1, c_d1, y_d1, color = :red, linewidth = 4) 
   lines!(ax3D, c_d2, x_d2, y_d2, color = :blue, linewidth = 4)
 
+  val = @lift(parameterisation($drivers, $parameters, constants))
+  point3D = @lift(Vec3f.($(drivers_vals[1]), $(drivers_vals[2]), $val))
+  scatter!(ax3D, point3D, color = val, markersize = 40, colormap = Reverse(:Spectral), colorrange = output.range,
+          strokewidth = 10, strokecolor = :black) # stroke not supported in WGLMakie?
+
   DataInspector(fig)
 
-  return fig  
+  return fig, val  
 end
 
 function webapp(parameterisation, inputs, output)
@@ -52,10 +60,11 @@ function webapp(parameterisation, inputs, output)
     n_parameters = length(inputs.parameters.values)
     drivers_sliders = [Slider(range(inputs.drivers.ranges[i][1], inputs.drivers.ranges[i][2], 10)) for i in 1:n_drivers] |> Tuple
     parameters_sliders = [Slider(range(inputs.parameters.ranges[i][1], inputs.parameters.ranges[i][2], 10)) for i in 1:n_parameters] |> Tuple
-    fig = param_dashboard(parameterisation, inputs, drivers_sliders, parameters_sliders, output)
+    fig, out = param_dashboard(parameterisation, inputs, drivers_sliders, parameters_sliders, output)
     drivers_sliders_UI = [DOM.div(string(inputs.drivers.names[i], " = "), drivers_sliders[i], @lift(round($(drivers_sliders[i].value), sigdigits = 2))) for i in 1:n_drivers]
     parameters_sliders_UI = [DOM.div(string(inputs.parameters.names[i], " = "), parameters_sliders[i], @lift(round($(parameters_sliders[i].value), sigdigits = 2))) for i in 1:n_parameters]
-    return DOM.div(drivers_sliders_UI..., parameters_sliders_UI..., fig)  
+    param_value = DOM.div(output.name, " = ", @lift(round($(out), sigdigits = 2)))
+    return DOM.div(drivers_sliders_UI..., parameters_sliders_UI..., param_value, fig)  
   end
   return Param_app
 end
