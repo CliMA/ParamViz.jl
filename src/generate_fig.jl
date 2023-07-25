01bf654f41d152906824c4ecc7b2e5296c1bced4
AlexisRenchon <a.renchon@gmail.com>
2023-07-25 11:41:14 -0700

    unit scaler seem to work

--- src/generate_fig.jl
+++ src/generate_fig.jl
@@ -8,9 +8,9 @@ function param_dashboard(parameterisation::Function, inputs::Inputs, drivers_sli
   fig = Figure(resolution = (800, 800))
 
   # JSServe layout
-  ax3D = Axis3(fig[1,1:2][1,1], xlabel = inputs.drivers.names[1], ylabel = inputs.drivers.names[2], zlabel = output.name); zlims!(ax3D, output.range)
-  ax_d1 = Axis(fig[2,1], xlabel = inputs.drivers.names[1], ylabel = output.name); ylims!(ax_d1, output.range)
-  ax_d2 = Axis(fig[2,2], xlabel = inputs.drivers.names[2], ylabel = output.name); ylims!(ax_d2, output.range)
+  ax3D = Axis3(fig[1,1:2][1,1], xlabel = inputs.drivers.names[1], ylabel = inputs.drivers.names[2], zlabel = output.name); zlims!(ax3D, output.range.*output.scaler)
+  ax_d1 = Axis(fig[2,1], xlabel = inputs.drivers.names[1], ylabel = output.name); ylims!(ax_d1, output.range.*output.scaler)
+  ax_d2 = Axis(fig[2,2], xlabel = inputs.drivers.names[2], ylabel = output.name); ylims!(ax_d2, output.range.*output.scaler)
 
   n_drivers = 2  
   n_parameters = length(inputs.parameters.values)
@@ -22,23 +22,23 @@ function param_dashboard(parameterisation::Function, inputs::Inputs, drivers_sli
   parameters = lift((args...,) -> args, parameters_vals...)
   constants = inputs.constants.values
 
-  x_d1 = collect(range(inputs.drivers.ranges[1][1], inputs.drivers.ranges[1][2], steps)) # min d1 to max d1, n steps
-  x_d2 = collect(range(inputs.drivers.ranges[2][1], inputs.drivers.ranges[2][2], steps)) # min d2 to max d2, n steps
-  c_d1 = @lift(repeat([$(drivers_vals[2])], steps)) # constant d1 val, length n
-  c_d2 = @lift(repeat([$(drivers_vals[1])], steps)) # constant d2 val, length n
-  y_d1 = @lift(d1_vec($(drivers_vals[2]), parameterisation, inputs, $parameters, steps)) # function output at constant driver 1
-  y_d2 = @lift(d2_vec($(drivers_vals[1]), parameterisation, inputs, $parameters, steps)) # function output at constant driver 2
+  x_d1 = collect(range(inputs.drivers.ranges[1][1].*inputs.drivers.scalers[1], inputs.drivers.ranges[1][2].*inputs.drivers.scalers[1], steps)) # min d1 to max d1, n steps
+  x_d2 = collect(range(inputs.drivers.ranges[2][1].*inputs.drivers.scalers[2], inputs.drivers.ranges[2][2].*inputs.drivers.scalers[2], steps)) # min d2 to max d2, n steps
+  c_d1 = @lift(repeat([$(drivers_vals[2])], steps).*inputs.drivers.scalers[2]) # constant d1 val, length n
+  c_d2 = @lift(repeat([$(drivers_vals[1])], steps).*inputs.drivers.scalers[1]) # constant d2 val, length n
+  y_d1 = @lift(d1_vec($(drivers_vals[2]), parameterisation, inputs, $parameters, steps).*output.scaler) # function output at constant driver 1
+  y_d2 = @lift(d2_vec($(drivers_vals[1]), parameterisation, inputs, $parameters, steps).*output.scaler) # function output at constant driver 2
   val = @lift(parameterisation($drivers, $parameters, constants).*output.scaler)
-  point3D = @lift(Vec3f.($(drivers_vals[1]), $(drivers_vals[2]), $val))
-  point2D_ax1 = @lift(Vec2f.($(drivers_vals[1]), $val))
-  point2D_ax2 = @lift(Vec2f.($(drivers_vals[2]), $val))
+  point3D = @lift(Vec3f.($(drivers_vals[1]).*inputs.drivers.scalers[1], $(drivers_vals[2]).*inputs.drivers.scalers[2], $val))
+  point2D_ax1 = @lift(Vec2f.($(drivers_vals[1]).*inputs.drivers.scalers[1], $val))
+  point2D_ax2 = @lift(Vec2f.($(drivers_vals[2]).*inputs.drivers.scalers[2], $val))
 
   # Plot 3D surface of model(drivers, params)
-  x = @lift(mat(parameterisation, inputs, $parameters, steps)[1]) 
-  y = @lift(mat(parameterisation, inputs, $parameters, steps)[2])
-  z = @lift(mat(parameterisation, inputs, $parameters, steps)[3])
+  x = @lift(mat(parameterisation, inputs, $parameters, steps)[1].*inputs.drivers.scalers[1]) 
+  y = @lift(mat(parameterisation, inputs, $parameters, steps)[2].*inputs.drivers.scalers[2])
+  z = @lift(mat(parameterisation, inputs, $parameters, steps)[3].*output.scaler)
   surface!(ax3D, x, y, z, colormap = Reverse((:Spectral, 0.8),), transparency = true, alpha = 0.2, shading = false, colorrange = output.range) 
-  cb = Colorbar(fig[1, 1:2][1, 2], colormap = Reverse(:Spectral), limits = output.range, label = output.name)
+  cb = Colorbar(fig[1, 1:2][1, 2], colormap = Reverse(:Spectral), limits = output.range.*output.scaler, label = output.name)
   cb.alignmode = Mixed(right = 0)
 
   # Plot 2D lines of model(drivers, params)
