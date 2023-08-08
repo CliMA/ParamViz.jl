649703726b4a39ec7b32efde6c63a5eadb0148c3
AlexisRenchon <a.renchon@gmail.com>
2023-08-08 14:05:22 -0700

    WIP

--- src/generate_fig.jl
+++ src/generate_fig.jl
@@ -7,8 +7,8 @@ where the user can interact with driver and parameter values via sliders.
 function param_dashboard(parameterisation::Function, inputs::Inputs, drivers_sliders, parameters_sliders, output)
   fig = Figure(resolution = (800, 800))
 
-  drivers_ranges_unitconverted = [ustrip.(uconvert.(drivers.units[i][2], (drivers.ranges[i])drivers.units[i][1])) for i = 1:2]
-  parameters_ranges_unitconverted = [ustrip.(uconvert.(parameters.units[i][2], (parameters.ranges[i])parameters.units[i][1])) for i = 1:length(parameters.units)]
+  drivers_ranges_unitconverted = [ustrip.(uconvert.(inputs.drivers.units[i][2], (inputs.drivers.ranges[i])inputs.drivers.units[i][1])) for i = 1:2]
+  parameters_ranges_unitconverted = [ustrip.(uconvert.(inputs.parameters.units[i][2], (inputs.parameters.ranges[i])inputs.parameters.units[i][1])) for i = 1:length(inputs.parameters.units)]
   output_range_unitconverted = ustrip.(uconvert.(output.unit[2], (output.range)output.unit[1]))
 
   # JSServe layout
@@ -18,8 +18,8 @@ function param_dashboard(parameterisation::Function, inputs::Inputs, drivers_sli
 
   n_drivers = 2  
   n_parameters = length(inputs.parameters.names)
-  drivers_vals = [@lift($(drivers_sliders[i].value)./inputs.drivers.scalers[i]) for i in 1:n_drivers] |> Tuple 
-  parameters_vals = [@lift($(parameters_sliders[i].value)./inputs.parameters.scalers[i]) for i in 1:n_parameters] |> Tuple 
+  drivers_vals = [@lift(ustrip.(uconvert.(inputs.drivers.units[i][1], ($(drivers_sliders[i].value))*inputs.drivers.units[i][2]))) for i in 1:n_drivers] |> Tuple
+  parameters_vals = [@lift(ustrip.(uconvert.(inputs.parameters.units[i][1], ($(parameters_sliders[i].value))*inputs.parameters.units[i][2]))) for i in 1:n_parameters] |> Tuple 
 
   steps = 30
   drivers = lift((args...,) -> args, drivers_vals...)
@@ -28,19 +28,20 @@ function param_dashboard(parameterisation::Function, inputs::Inputs, drivers_sli
 
   x_d1 = collect(range(drivers_ranges_unitconverted[1][1], drivers_ranges_unitconverted[1][2], steps)) # min d1 to max d1, n steps
   x_d2 = collect(range(drivers_ranges_unitconverted[2][1], drivers_ranges_unitconverted[2][2], steps)) # min d2 to max d2, n steps
-  c_d1 = @lift(repeat([$(drivers_vals[2])], steps).*inputs.drivers.scalers[2]) # constant d1 val, length n
-  c_d2 = @lift(repeat([$(drivers_vals[1])], steps).*inputs.drivers.scalers[1]) # constant d2 val, length n
-  y_d1 = @lift(d1_vec($(drivers_vals[2]), parameterisation, inputs, $parameters, steps).*output.scaler) # function output at constant driver 1
-  y_d2 = @lift(d2_vec($(drivers_vals[1]), parameterisation, inputs, $parameters, steps).*output.scaler) # function output at constant driver 2
-  val = @lift(parameterisation($drivers, $parameters, constants).*output.scaler)
-  point3D = @lift(Vec3f.($(drivers_vals[1]).*inputs.drivers.scalers[1], $(drivers_vals[2]).*inputs.drivers.scalers[2], $val))
-  point2D_ax1 = @lift(Vec2f.($(drivers_vals[1]).*inputs.drivers.scalers[1], $val))
-  point2D_ax2 = @lift(Vec2f.($(drivers_vals[2]).*inputs.drivers.scalers[2], $val))
+  c_d1 = @lift(repeat([ustrip.(uconvert.(inputs.drivers.units[2][2], ($(drivers_vals[2])*inputs.drivers.units[2][1])))], steps)) # constant d1 val, length n
+  c_d2 = @lift(repeat([ustrip.(uconvert.(inputs.drivers.units[1][2], ($(drivers_vals[1])*inputs.drivers.units[1][1])))], steps)) # constant d2 val, length n
+  y_d1 = @lift(ustrip.(uconvert.(output.unit[2], (d1_vec($(drivers_vals[2]), parameterisation, inputs, $parameters, steps))*output.unit[1]))) # function output at constant driver 1
+  y_d2 = @lift(ustrip.(uconvert.(output.unit[2], (d1_vec($(drivers_vals[1]), parameterisation, inputs, $parameters, steps))*output.unit[1]))) # function output at constant driver 2
+  val = @lift(ustrip.(uconvert.(output.unit[2], (parameterisation($drivers, $parameters, constants))*output.unit[1])))
+  point3D = @lift(Vec3f.(ustrip.(uconvert.(inputs.drivers.units[1][2], ($(drivers_vals[1]))*inputs.drivers.units[1][1])), ustrip.(uconvert.(inputs.drivers.units[2][2], ($(drivers_vals[2]))*inputs.drivers.units[2][1])), $val))
+  point2D_ax1 = @lift(Vec2f.(ustrip.(uconvert.(inputs.drivers.units[1][2], ($(drivers_vals[1]))*inputs.drivers.units[1][1])), $val))
+  point2D_ax2 = @lift(Vec2f.(ustrip.(uconvert.(inputs.drivers.units[2][2], ($(drivers_vals[2]))*inputs.drivers.units[2][1])), $val))
 
   # Plot 3D surface of model(drivers, params)
-  x = @lift(mat(parameterisation, inputs, $parameters, steps)[1].*inputs.drivers.scalers[1]) 
-  y = @lift(mat(parameterisation, inputs, $parameters, steps)[2].*inputs.drivers.scalers[2])
-  z = @lift(mat(parameterisation, inputs, $parameters, steps)[3].*output.scaler)
+  
+  x = @lift(ustrip.(uconvert.(inputs.drivers.units[1][2], (mat(parameterisation, inputs, $parameters, steps)[1])*inputs.drivers.units[1][1])))
+  y = @lift(ustrip.(uconvert.(inputs.drivers.units[2][2], (mat(parameterisation, inputs, $parameters, steps)[2])*inputs.drivers.units[2][1])))
+  z = @lift(ustrip.(uconvert.(output.unit[2], (mat(parameterisation, inputs, $parameters, steps)[3])*output.unit[1])))
   surface!(ax3D, x, y, z, colormap = Reverse((:Spectral, 0.8),), transparency = true, alpha = 0.2, shading = false, colorrange = output_range_unitconverted) 
   cb = Colorbar(fig[1, 1:2][1, 2], colormap = Reverse(:Spectral), limits = output.range.*output.scaler, label = output.name)
   cb.alignmode = Mixed(right = 0)
@@ -63,11 +64,14 @@ function param_dashboard(parameterisation::Function, inputs::Inputs, drivers_sli
 end
 
 function webapp(parameterisation, inputs, output)
-  Param_app = App() do 
+  Param_app = App() do
+    drivers_ranges_unitconverted = [ustrip.(uconvert.(inputs.drivers.units[i][2], (inputs.drivers.ranges[i])inputs.drivers.units[i][1])) for i = 1:2]
+    parameters_ranges_unitconverted = [ustrip.(uconvert.(inputs.parameters.units[i][2], (inputs.parameters.ranges[i])inputs.parameters.units[i][1])) for i = 1:length(inputs.parameters.units)]
+    output_range_unitconverted = ustrip.(uconvert.(output.unit[2], (output.range)output.unit[1]))
     n_drivers = 2  
     n_parameters = length(inputs.parameters.names)
-    drivers_range = [round.(range(inputs.drivers.ranges[i][1].*inputs.drivers.scalers[i], inputs.drivers.ranges[i][2].*inputs.drivers.scalers[i], 12), sigdigits = 2) for i in 1:n_drivers]
-    parameters_range = [round.(range(inputs.parameters.ranges[i][1].*inputs.parameters.scalers[i], inputs.parameters.ranges[i][2].*inputs.parameters.scalers[i], 12), sigdigits = 2) for i in 1:n_parameters]
+    drivers_range = [round.(range(drivers_ranges_unitconverted[i][1], drivers_ranges_unitconverted[i][2], 12), sigdigits = 2) for i in 1:n_drivers]
+    parameters_range = [round.(range(parameters_ranges_unitconverted[i][1], parameters_ranges_unitconverted[i][2], 12), sigdigits = 2) for i in 1:n_parameters]
     drivers_sliders = [JSServe.TailwindDashboard.Slider(inputs.drivers.names[i], drivers_range[i], value = drivers_range[i][6]) for i in 1:n_drivers] |> Tuple
     parameters_sliders = [JSServe.TailwindDashboard.Slider(inputs.parameters.names[i], parameters_range[i], value = parameters_range[i][6]) for i in 1:n_parameters] |> Tuple
     fig, out = param_dashboard(parameterisation, inputs, drivers_sliders, parameters_sliders, output)
@@ -93,24 +97,27 @@ end
 #= with units
 using ParamViz
 using JSServe
+using WGLMakie
 
 using Unitful: R, L, mol, K, kJ, °C, m, g, cm, hr, mg, s, μmol
 using UnitfulMoles: molC
 using Unitful, UnitfulMoles
 @compound CO₂
 
+using ClimaLSM
+using ClimaLSM.Canopy
+FT = Float64
+
 drivers = Drivers(("PAR (μmol m⁻² s⁻¹))", "LAI (m² m⁻²))"),
-                  FT.((500 * 1e-6, 5)),
                   (FT.([0, 1500 * 1e-6]), FT.([0, 10])),
-                  (1e6, 1.0) # scalers
+                  ((mol*m^-2*s^-1, μmol*m^-2*s^-1), (m^2*m^-2, m^2*m^-2))
                  )
 
 parameters = Parameters(("canopy reflectance, ρ_leaf",
                          "extinction coefficient, K",
                          "clumping index, Ω"),
-                        (FT(0.1), FT(0.6), FT(0.69)),
                         (FT.([0, 1]), FT.([0, 1]), FT.([0, 1])),
-                        (1, 1, 1) # scalers
+                        ((m, m), (m, m), (m, m)) # dummy units, no conversion
                        )
 
 # need a method with no constant! 
@@ -119,7 +126,7 @@ constants = Constants(("a", "b"), (FT(1), FT(2)))
 
 inputs = Inputs(drivers, parameters, constants)
 
-output = Output("APAR (μmol m⁻² s⁻¹)", [0, 1500 * 1e-6], 1e6)
+output = Output("APAR (μmol m⁻² s⁻¹)", [0, 1500 * 1e-6], (mol*m^-2*s^-1, μmol*m^-2*s^-1))
 
 import ParamViz.parameterisation
 function parameterisation(PAR, LAI, ρ_leaf, K, Ω, a, b)   
